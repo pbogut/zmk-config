4681666d04647c42f9f383bc4994a4e0995906b2
Pawel Bogut <pbogut@pbogut.me>
2024-01-11 18:13:01 +0100

    feat: add dactyl_gaming board
    My handwire half of dactyl manuform used for gaming.

--- helper.sh
+++ helper.sh
@@ -9,21 +9,22 @@ cd "$dir" || exit 1
 
 action=""
 half="both"
+kb=""
 
 usage() {
     echo "Ussage: ${0##*/} <ACTION> [OPTIONS]"
     echo ""
     echo "Action:"
-    echo "  init           initlaize"
-    echo "  deps           install dependencies"
-    echo "  build [half]   build firmware [half: both,left,right] default: both"
-    echo "  copy [half]    build firmware [half: left,right]"
-    echo "  cleanup        cleanup"
+    echo "  init                 initlaize"
+    echo "  deps                 install dependencies"
+    echo "  build [kb]           build firmware for keyboard [kb: kyria,dactyl_gaming]"
+    echo "  copy  [kb]           install firmware for keyboard [kb: kyria,dactyl_gaming]"
+    echo "  cleanup              cleanup"
     echo ""
     echo "Options:"
-    echo "  --left         perform action for left half"
-    echo "  --right        perform action for right half"
-    echo "  -h, --help     display this help and exit"
+    echo "  --left               perform action for left half"
+    echo "  --right              perform action for right half"
+    echo "  -h, --help           display this help and exit"
 }
 
 while test $# -gt 0; do
@@ -51,9 +52,13 @@ while test $# -gt 0; do
         build)
             action="build"
             shift
+            kb="$1"
+            shift
             ;;
         copy)
             action="copy"
+            shift
+            kb="$1"
             shift
             ;;
         cleanup)
@@ -81,6 +86,9 @@ if [[ $action == "deps" ]]; then
 fi
 
 if [[ $action == "init" ]]; then
+    if [[ -d "$dir/zmk" ]]; then
+        git -C "$dir/zmk" reset --hard
+    fi
     west init -l config
     west update
     west zephyr-export
@@ -99,39 +107,67 @@ if [[ $action == "cleanup" ]]; then
 fi
 
 if [[ $action == "copy" ]]; then
-    if [[ $half == "both" ]] || [[ $half == "left" ]]; then
-        while ! cp ./kyria_left-nice_nano_v2-zmk.uf2 /run/media/pbogut/NICENANO/ 2> /dev/null; do
-            echo "Waiting for device [left] ..."
-            sleep 1s
-        done
-        echo "Done"
+    if [[ $kb == "" ]]; then
+        echo "Keyboard not specified"
+        exit 1
     fi
+    case "$kb" in
+        "kyria")
+            if [[ $half == "both" ]] || [[ $half == "left" ]]; then
+                while ! cp ./kyria_left-nice_nano_v2-zmk.uf2 /run/media/pbogut/NICENANO/ 2> /dev/null; do
+                    echo "Waiting for device [left] ..."
+                    sleep 1s
+                done
+                echo "Done"
+            fi
 
-    if [[ $half == "both" ]]; then
-        sleep 2s
-    fi
+            if [[ $half == "both" ]]; then
+                sleep 2s
+            fi
 
-    if [[ $half == "both" ]] || [[ $half == "right" ]]; then
-        while ! cp ./kyria_right-nice_nano_v2-zmk.uf2 /run/media/pbogut/NICENANO/ 2> /dev/null; do
-            echo "Waiting for device [right] ..."
-            sleep 1s
-        done
-        echo "Done"
-    fi
+            if [[ $half == "both" ]] || [[ $half == "right" ]]; then
+                while ! cp ./kyria_right-nice_nano_v2-zmk.uf2 /run/media/pbogut/NICENANO/ 2> /dev/null; do
+                    echo "Waiting for device [right] ..."
+                    sleep 1s
+                done
+                echo "Done"
+            fi
+            ;;
+        "dactyl_gaming")
+            while ! cp ./dactyl_gaming-nice_nano_v2-zmk.uf2 /run/media/pbogut/NICENANO/ 2> /dev/null; do
+                echo "Waiting for device [dactyl_gaming] ..."
+                sleep 1s
+            done
+            ;;
+    esac
 fi
 
 if [[ $action == "build" ]]; then
-    if [[ $half == "both" ]] || [[ $half == "left" ]]; then
-        rm build -fr
-        mkdir -p ./build/artifacts
-        west build -s zmk/app -b "nice_nano_v2" -- -DZMK_CONFIG="$PWD/config" -DSHIELD="kyria_left nice_view_adapter nice_view" &&
-            cp build/zephyr/zmk.uf2 kyria_left-nice_nano_v2-zmk.uf2
+    if [[ $kb == "" ]]; then
+        echo "Keyboard not specified"
+        exit 1
     fi
-    if [[ $half == "both" ]] || [[ $half == "right" ]]; then
-        rm build -fr
-        mkdir -p ./build/artifacts
-        west build -s zmk/app -b "nice_nano_v2" -- -DZMK_CONFIG="$PWD/config" -DSHIELD="kyria_right nice_view_adapter nice_view" &&
-            cp build/zephyr/zmk.uf2 kyria_right-nice_nano_v2-zmk.uf2
+    case "$kb" in
+        "kyria")
+            if [[ $half == "both" ]] || [[ $half == "left" ]]; then
+                rm build -fr
+                mkdir -p ./build/artifacts
+                west build -s zmk/app -b "nice_nano_v2" -- -DZMK_CONFIG="$PWD/config" -DSHIELD="kyria_left nice_view_adapter nice_view" &&
+                    cp build/zephyr/zmk.uf2 kyria_left-nice_nano_v2-zmk.uf2
+            fi
+            if [[ $half == "both" ]] || [[ $half == "right" ]]; then
+                rm build -fr
+                mkdir -p ./build/artifacts
+                west build -s zmk/app -b "nice_nano_v2" -- -DZMK_CONFIG="$PWD/config" -DSHIELD="kyria_right nice_view_adapter nice_view" &&
+                    cp build/zephyr/zmk.uf2 kyria_right-nice_nano_v2-zmk.uf2
 
-    fi
+            fi
+            ;;
+        "dactyl_gaming")
+            rm build -fr
+            mkdir -p ./build/artifacts
+            west build -s zmk/app -b "nice_nano_v2" -- -DZMK_CONFIG="$PWD/config" -DSHIELD="dactyl_gaming" -DZMK_EXTRA_MODULES="$PWD/shields/dactyl_gaming" &&
+                cp build/zephyr/zmk.uf2 dactyl_gaming-nice_nano_v2-zmk.uf2
+            ;;
+    esac
 fi
